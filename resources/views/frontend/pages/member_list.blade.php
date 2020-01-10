b54db49bf71edcb605424130e6db00d16d27431c
prothomrobi <prothomrobi1@gmail.com>
2020-01-10 21:57:34 +0600

    fisinf member list design

--- resources/views/frontend/pages/member_list.blade.php
+++ resources/views/frontend/pages/member_list.blade.php
@@ -19,18 +19,133 @@
     <section class="site-section single_blog_area p-t-70" id="gallery-section">
         <div class="container">
             <div id="posts" class="row no-gutter">
-                <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
-                    <a href="frontend/img/blog-img/blog-4.jpg" class="item-wrap fancybox">
-                        <span class="icon-search2"></span>
-                        <img class="img-fluid" src="frontend/img/blog-img/blog-4.jpg">
-                    </a>
+{{--                <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">--}}
+{{--                    <a href="frontend/img/blog-img/blog-4.jpg" class="item-wrap fancybox">--}}
+{{--                        <span class="icon-search2"></span>--}}
+{{--                        <img class="img-fluid" src="frontend/img/blog-img/blog-4.jpg">--}}
+{{--                    </a>--}}
+{{--                </div>--}}
+                <div class="item col-sm-2 m-b-10">
+                    <article class="clearfix mb-sm-30 faculty-member-image-box">
+                        <img src="https://nstu.edu.bd/uploads/avatar/KcGhSKgwLr1553895871.jpg"
+                             alt=""
+                             class="img-responsive img-fullwidth">
+
+                        <div class="media-body faculty-title">
+                            <h6 class="text-info">Dr. Humayun Kabir</h6>
+                            <div class="details m-t-30">
+
+                                <a href="http://127.0.0.1:8000/about" class="btn fancy-btn fancy-active" style="width: 140px;height: 40px">Details</a>
+                            </div>
+                        </div>
+                    </article>
                 </div>
-                <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
-                    <a href="frontend/img/bg-img/convocation.jpg" class="item-wrap fancybox">
-                        <span class="icon-search2"></span>
-                        <img class="img-fluid" src="frontend/img/bg-img/convocation.jpg">
-                    </a>
+                <div class="item col-sm-2 m-b-10">
+                    <article class="clearfix mb-sm-30 faculty-member-image-box">
+                        <img src="https://nstu.edu.bd/uploads/avatar/KcGhSKgwLr1553895871.jpg"
+                             alt=""
+                             class="img-responsive img-fullwidth">
+
+                        <div class="media-body faculty-title">
+                            <h6 class="text-info">Dr. Humayun Kabir</h6>
+                            <div class="details m-t-30">
+
+                                <a href="http://127.0.0.1:8000/about" class="btn fancy-btn fancy-active" style="width: 140px;height: 40px">Details</a>
+                            </div>
+                        </div>
+                    </article>
                 </div>
+                <div class="item col-sm-2 m-b-10">
+                    <article class="clearfix mb-sm-30 faculty-member-image-box">
+                        <img src="https://nstu.edu.bd/uploads/avatar/KcGhSKgwLr1553895871.jpg"
+                             alt=""
+                             class="img-responsive img-fullwidth">
+
+                        <div class="media-body faculty-title">
+                            <h6 class="text-info">Dr. Humayun Kabir</h6>
+                            <div class="details m-t-30">
+
+                                <a href="http://127.0.0.1:8000/about" class="btn fancy-btn fancy-active" style="width: 140px;height: 40px">Details</a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="item col-sm-2 m-b-10">
+                    <article class="clearfix mb-sm-30 faculty-member-image-box">
+                        <img src="https://nstu.edu.bd/uploads/avatar/KcGhSKgwLr1553895871.jpg"
+                             alt=""
+                             class="img-responsive img-fullwidth">
+
+                        <div class="media-body faculty-title">
+                            <h6 class="text-info">Dr. Humayun Kabir</h6>
+                            <div class="details m-t-30">
+
+                                <a href="http://127.0.0.1:8000/about" class="btn fancy-btn fancy-active" style="width: 140px;height: 40px">Details</a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="item col-sm-2 m-b-10">
+                    <article class="clearfix mb-sm-30 faculty-member-image-box">
+                        <img src="https://nstu.edu.bd/uploads/avatar/KcGhSKgwLr1553895871.jpg"
+                             alt=""
+                             class="img-responsive img-fullwidth">
+
+                        <div class="media-body faculty-title">
+                            <h6 class="text-info">Dr. Humayun Kabir</h6>
+                            <div class="details m-t-30">
+
+                                <a href="http://127.0.0.1:8000/about" class="btn fancy-btn fancy-active" style="width: 140px;height: 40px">Details</a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="item col-sm-2 m-b-10">
+                    <article class="clearfix mb-sm-30 faculty-member-image-box">
+                        <img src="https://nstu.edu.bd/uploads/avatar/KcGhSKgwLr1553895871.jpg"
+                             alt=""
+                             class="img-responsive img-fullwidth">
+
+                        <div class="media-body faculty-title">
+                            <h6 class="text-info">Dr. Humayun Kabir</h6>
+                            <div class="details m-t-30">
+
+                                <a href="http://127.0.0.1:8000/about" class="btn fancy-btn fancy-active" style="width: 140px;height: 40px">Details</a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="item col-sm-2 m-b-10">
+                    <article class="clearfix mb-sm-30 faculty-member-image-box">
+                        <img src="https://nstu.edu.bd/uploads/avatar/KcGhSKgwLr1553895871.jpg"
+                             alt=""
+                             class="img-responsive img-fullwidth">
+
+                        <div class="media-body faculty-title">
+                            <h6 class="text-info">Dr. Humayun Kabir</h6>
+                            <div class="details m-t-30">
+
+                                <a href="http://127.0.0.1:8000/about" class="btn fancy-btn fancy-active" style="width: 140px;height: 40px">Details</a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div class="item col-sm-2 m-b-10">
+                    <article class="clearfix mb-sm-30 faculty-member-image-box">
+                        <img src="https://nstu.edu.bd/uploads/avatar/KcGhSKgwLr1553895871.jpg"
+                             alt=""
+                             class="img-responsive img-fullwidth">
+
+                        <div class="media-body faculty-title">
+                            <h6 class="text-info">Dr. Humayun Kabir</h6>
+                            <div class="details m-t-30">
+
+                                <a href="http://127.0.0.1:8000/about" class="btn fancy-btn fancy-active" style="width: 140px;height: 40px">Details</a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+
             </div>
         </div>
     </section>
