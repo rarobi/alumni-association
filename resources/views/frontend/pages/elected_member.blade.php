214b96fb47ba79f2a687451fc9c877aae9d183e1
prothomrobi <prothomrobi1@gmail.com>
2020-01-19 20:58:22 +0600

    working on elected members

--- resources/views/frontend/pages/elected_member.blade.php
+++ resources/views/frontend/pages/elected_member.blade.php
@@ -19,44 +19,23 @@
     <section class="single_blog_area p-t-70">
         <div class="container">
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-sm-6 offset-sm-3">
                     <div class="accordion elected-members">
                         <div class="heading">
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="head-img">
-                                        <img src="frontend/img/blog-img/blog-4.jpg" alt="" style="height: 150px">
+                                        <img src="frontend/img/clients-img/azad.jpg" alt="" style="height: 150px">
                                     </div>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="head-cont">
-                                        <h4>MOHAMMAD BABUL MIAH</h4>
-                                        <p>Company Secretary & Founder Shareholder</p>
-                                        <p>Batch: 01</p>
+                                        <h4>ABUL KALAM AZAD</h4>
+                                        <h6 class="text-info"> PRESIDENT</h6>
+                                        <p>BATCH: 01</p>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="accordion elected-members">
-                        <div class="heading">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="head-img">
-                                        <img src="frontend/img/blog-img/blog-4.jpg" alt="" style="height: 150px">
-                                    </div>
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="head-cont">
-                                        <h4>MOHAMMAD BABUL MIAH</h4>
-                                        <p>Company Secretary & Founder Shareholder</p>
-                                        <p>Batch: 01</p>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
                     </div>
                 </div>
@@ -68,14 +47,14 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="head-img">
-                                        <img src="frontend/img/blog-img/blog-4.jpg" alt="" style="height: 150px">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
                                     </div>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="head-cont">
-                                        <h4>MOHAMMAD BABUL MIAH</h4>
-                                        <p>Company Secretary & Founder Shareholder</p>
-                                        <p>Batch: 01</p>
+                                        <h4>MD. SAIFUR RAHMAN (SAIF)</h4>
+                                        <h6 class="text-info">VICE - PRESIDENT</h6>
+                                        <p>BATCH: 02</p>
                                     </div>
                                 </div>
                             </div>
@@ -88,18 +67,101 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="head-img">
-                                        <img src="frontend/img/blog-img/blog-4.jpg" alt="" style="height: 150px">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
                                     </div>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="head-cont">
-                                        <h4>MOHAMMAD BABUL MIAH</h4>
-                                        <p>Company Secretary & Founder Shareholder</p>
-                                        <p>Batch: 01</p>
+                                        <h4>BISHWA JIT GHOSH</h4>
+                                        <h6 class="text-info"> VICE - PRESIDENT</h6>
+                                        <p>BATCH: 01</p>
                                     </div>
                                 </div>
                             </div>
-
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>MARUF HASAN BULBUL</h4>
+                                        <h6 class="text-info">GENERAL SECRETARY</h6>
+                                        <p>BATCH: 02</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>MD MAINUDDIN</h4>
+                                        <h6 class="text-info"> TREASURER</h6>
+                                        <p>BATCH: 01</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>MD. FAHIDUL ISLAM (SHAON)</h4>
+                                        <h6 class="text-info">ASSISTANT GENERAL SECRETARY</h6>
+                                        <p>BATCH: 04 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>HASNAT RIAZ</h4>
+                                        <h6 class="text-info"> ASSISTANT GENERAL SECRETARY</h6>
+                                        <p>BATCH: 04</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
