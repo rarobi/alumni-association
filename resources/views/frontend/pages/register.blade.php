d7af1a41ec72335cad560b041fcad6172421ebf5
Robi412 <robi.hislbd@gmail.com>
2020-02-06 11:59:21 +0600

    add alert close

--- resources/views/frontend/pages/register.blade.php
+++ resources/views/frontend/pages/register.blade.php
@@ -23,7 +23,8 @@
                     @endforeach
                 @endif
             @if(session()->has('message'))
-                <div class="alert alert-success text-center">
+                <div class="alert alert-success text-center alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     {{ session()->get('message') }}
                 </div>
             @endif
