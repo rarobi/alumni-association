ba2043ed5ac6f1c64330cce2233841c310c0f38b
Md. Robiul Alam <robi.hislbd@gmail.com>
2020-03-25 09:02:32 +0600

    added elected members

--- resources/views/frontend/pages/elected_member.blade.php
+++ resources/views/frontend/pages/elected_member.blade.php
@@ -166,6 +166,384 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>A.Q.M Salauddin Pathan</h4>
+                                        <h6 class="text-info">ORGANIZING SECRETARY</h6>
+                                        <p>BATCH: 06 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Md. Hasibur Rahman</h4>
+                                        <h6 class="text-info"> COMMUNICATION & PUBLICATION SECRETARY</h6>
+                                        <p>BATCH: 06</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Md Yasin Kabir</h4>
+                                        <h6 class="text-info">EDUCATION,TRAINING & RESEARCH SECRETARY</h6>
+                                        <p>BATCH: 04 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Md.Auhidur Rahman Sumon</h4>
+                                        <h6 class="text-info"> SOCIAL WELFARE SECRETARY</h6>
+                                        <p>BATCH: 04</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Md. Mehedi Hasan Mahi</h4>
+                                        <h6 class="text-info">CULTURAL SECRETARY</h6>
+                                        <p>BATCH: 01 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Nishu Nath</h4>
+                                        <h6 class="text-info"> SPORTS & ENTERTAINMENT SECRETARY</h6>
+                                        <p>BATCH: 04</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Monjurul Alam Mohsin</h4>
+                                        <h6 class="text-info">EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 02 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Md. Farhat Bin Zaman</h4>
+                                        <h6 class="text-info"> EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 02</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Prince Faisal Ahmed</h4>
+                                        <h6 class="text-info">EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 02 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Tushar Kanthi Kar</h4>
+                                        <h6 class="text-info"> EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 02</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Israt Sharmin Jui</h4>
+                                        <h6 class="text-info">EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 04 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Tahsin-Ul-Abrar</h4>
+                                        <h6 class="text-info"> EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 04</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Akhlashur Rahman Mashpy</h4>
+                                        <h6 class="text-info">EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 05 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Mohammad Ismail Hossen</h4>
+                                        <h6 class="text-info"> EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 05</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Suvrajit Roy</h4>
+                                        <h6 class="text-info">EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 06 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Mausumi Akter</h4>
+                                        <h6 class="text-info"> EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 06</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Sayed Mahmud Raihan</h4>
+                                        <h6 class="text-info">EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 06 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="accordion elected-members">
+                        <div class="heading">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="head-img">
+                                        <img src="frontend/img/bg-img/dummy-profile.jpg" alt="" style="height: 150px">
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="head-cont">
+                                        <h4>Mohammad Rafid Shahriar</h4>
+                                        <h6 class="text-info"> EXECUTIVE MEMBER</h6>
+                                        <p>BATCH: 06</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <!-- ****** Single Blog Area End ****** -->
